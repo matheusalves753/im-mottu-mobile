b794aba065706b4e527a68718b82132e77a8fbaf
Matheus Alves <matheusalves753@gmail.com>
2024-07-02 23:32:52 -0300

    fix(api_authentication): Fix Marvel API authentication in RemoteDataSourceImpl

--- lib/src/data/datasources/remote/remote_data_source.dart
+++ lib/src/data/datasources/remote/remote_data_source.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:crypto/crypto.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:im_mottu_mobile/src/data/models/character_data_wrapper_model/character_data_wrapper_model.dart';
@@ -10,24 +11,40 @@ abstract interface class RemoteDataSource {
 class RemoteDataSourceImpl implements RemoteDataSource {
   final http.Client _client;
   final String _baseUrl = 'gateway.marvel.com';
-  final String _apiKey = dotenv.env['MARVEL_API_KEY']!;
+  final String _publicKey = dotenv.env['MARVEL_PUBLIC_KEY']!;
+  final String _privateKey = dotenv.env['MARVEL_PRIVATE_KEY']!;
 
   RemoteDataSourceImpl(this._client);
+
+  Map<String, String> _generateAuthParams() {
+    final timestamp = DateTime.now().millisecondsSinceEpoch.toString();
+    final hash = md5
+        .convert(utf8.encode('$timestamp$_privateKey$_publicKey'))
+        .toString();
+
+    return {
+      'apikey': _publicKey,
+      'ts': timestamp,
+      'hash': hash,
+    };
+  }
+
+  Uri _buildUrl(String path, Map<String, String> additionalParams) {
+    final authParams = _generateAuthParams();
+    final allParams = {...authParams, ...additionalParams};
+
+    return Uri.https(_baseUrl, path, allParams);
+  }
 
   @override
   Future<CharacterDataWrapperModel> getCharacters({
     int offset = 0,
     int limit = 20,
   }) async {
-    final uri = Uri.https(
-      _baseUrl,
-      '/v1/public/characters',
-      {
-        'apikey': _apiKey,
-        'offset': offset.toString(),
-        'limit': limit.toString(),
-      },
-    );
+    final uri = _buildUrl('/v1/public/characters', {
+      'offset': offset.toString(),
+      'limit': limit.toString(),
+    });
 
     try {
       final response = await _client.get(uri);
